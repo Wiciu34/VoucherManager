efe24e6dd17b249160203effd64912a6613c614b
Wiciu34 <wiktor.langier7@gmail.com>
2025-07-14 15:53:24 +0200

    Add migration

---
+++ VoucherManager/Migrations/20250714135139_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoucherManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attractions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attractions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vouchers",
+                columns: table => new
+                {
+                    SerialNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    VoucherType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    SellDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Resort = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vouchers", x => x.SerialNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AttractionVoucher",
+                columns: table => new
+                {
+                    AttractionsId = table.Column<int>(type: "int", nullable: false),
+                    VouchersSerialNumber = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttractionVoucher", x => new { x.AttractionsId, x.VouchersSerialNumber });
+                    table.ForeignKey(
+                        name: "FK_AttractionVoucher_Attractions_AttractionsId",
+                        column: x => x.AttractionsId,
+                        principalTable: "Attractions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AttractionVoucher_Vouchers_VouchersSerialNumber",
+                        column: x => x.VouchersSerialNumber,
+                        principalTable: "Vouchers",
+                        principalColumn: "SerialNumber",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttractionVoucher_VouchersSerialNumber",
+                table: "AttractionVoucher",
+                column: "VouchersSerialNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AttractionVoucher");
+
+            migrationBuilder.DropTable(
+                name: "Attractions");
+
+            migrationBuilder.DropTable(
+                name: "Vouchers");
+        }
+    }
+}
