edd60d787c1843ff2b5415992da022e490469a82
Wiciu34 <wiktor.langier7@gmail.com>
2025-07-14 14:14:26 +0200

    Add enum status to handle status property in voucher model

---
+++ VoucherManager/Data/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace VoucherManager.Data
+{
+    public enum Status
+    {
+        Aktywny,
+        Nieaktywny,
+        Zrealizowany,
+        Niezrealizowany,
+        Anulowany,
+    }
+}
