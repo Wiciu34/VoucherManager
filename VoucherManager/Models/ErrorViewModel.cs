ae45e313d5ba391c266053dc6b621dd812fead69
w.langier <w.langier@00L-090>
2025-07-14 11:58:04 +0200

    Dodaj pliki projektów.

---
+++ VoucherManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VoucherManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
