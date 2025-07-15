d7ad20b8539edf1ebc8bb9854405f9f3de05e0ee
Wiciu34 <wiktor.langier7@gmail.com>
2025-07-15 10:53:38 +0200

    Add Voucher Repository with implementations of GetAllVouchersAsyns method

---
+++ VoucherManager/Repositories/VoucherRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VoucherManager.Data;
+using VoucherManager.Interfaces;
+using VoucherManager.Models;
+
+namespace VoucherManager.Repositories;
+
+public class VoucherRepository : IVoucherRepository
+{
+    private readonly AppDbContext _context;
+    public VoucherRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Voucher>> GetAllVouchersAsync()
+    {
+        var vouchers = await _context.Vouchers.ToListAsync();
+        return vouchers;
+    }
+}
