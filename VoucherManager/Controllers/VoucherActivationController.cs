4f7de58b028e7853a0a6d42f5aab6024624cdeae
Wiciu34 <wiktor.langier7@gmail.com>
2025-07-16 14:12:23 +0200

    Add VoucherActivation controller with index view

---
+++ VoucherManager/Controllers/VoucherActivationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VoucherManager.Controllers;
+
+public class VoucherActivationController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- VoucherManager/Program.cs
+++ VoucherManager/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=VoucherActivation}/{action=Index}/{id?}");
 
 app.Run();
