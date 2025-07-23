8302a8fbb958e40ebc9c6e165dcda6e673ffdf91
Wiciu34 <wiktor.langier7@gmail.com>
2025-07-23 11:27:08 +0200

    Apply migration to change type of voucher type property in database

---
+++ VoucherManager/Migrations/20250723092536_VoucherTypeEnum.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoucherManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class VoucherTypeEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VoucherType",
+                table: "Vouchers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VoucherType",
+                table: "Vouchers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
